9bfa0902c497b59aa239c4637481e14cd0f70e51
“Pikadev1771” <pikadev1771@gmail.com>
2022-12-16 15:19:31 +0900

    feat: dev branch 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <div className="App">ToDo App 프로젝트 세팅 연습 🌈🦄✅🌟🔥</div>;
+  return (
+    <div className="App">
+      <h1>ToDo App 프로젝트 세팅 연습 🌈🦄✅🌟🔥</h1>
+      <h3>dev 브랜치 생성</h3>;
+    </div>
+  );
 }
 
 export default App;
